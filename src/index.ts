dc04910db7e91ec2a8f0edb96cc096b10b12d12f
Hound404 <wildhound404@gmail.com>
2021-10-27 00:34:57 +0600

    backend task1 completed

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Express } from "express";
+import { json } from "stream/consumers";
 import { getData } from "./db/db";
 
 const port = 3500;
@@ -6,11 +7,58 @@ const express = require("express");
 
 const app: Express = express();
 
-// console.log(db);
+function organizeData(data: any[], limit: number, offset: number) {
+  {
+    // @ts-ignore
+    data.sort((a: any, b: any) => {
+      if (a.ifsc < b.ifsc) return -1;
+      if (a.ifsc > b.ifsc) return 1;
+      return 0;
+    });
 
-// @ts-ignore
-getData("RTGS").then((data) => {
-  console.log(data);
+    if ((limit === 0 && offset === 0) || offset >= data.length) {
+      return data;
+    } else if (offset === 0) {
+      return data.slice(0, limit);
+    } else if (limit === 0) {
+      return data.slice(offset, data.length);
+    } else {
+      return data.slice(offset, offset + limit);
+    }
+  }
+}
+
+app.get("/api/branches/autocomplete", async (req, res) => {
+  const text: string | undefined = req.query.q?.toString();
+  const limit: string | undefined = req.query.limit?.toString();
+  const offset: string | undefined = req.query.offset?.toString();
+
+  // handling query error
+  if (!text) {
+    res.status(400).send(`invalid query parameter`);
+    return;
+  } else if (limit !== undefined && !parseInt(limit) && parseInt(limit) < 0) {
+    res.status(400).send(`invalid limit parameter`);
+    return;
+  } else if (
+    offset !== undefined &&
+    !parseInt(offset) &&
+    parseInt(offset) < 0
+  ) {
+    res.status(400).send(`invalid offset parameter`);
+    return;
+  }
+
+  // getting data from database
+  await getData(text).then((data) => {
+    // organizing data based of limit, offset & ifsc
+    const result = organizeData(
+      data,
+      limit === undefined ? 0 : parseInt(limit),
+      offset === undefined ? 0 : parseInt(offset)
+    );
+    res.send(JSON.stringify(result));
+  });
 });
 
 app.listen(port, () => {
